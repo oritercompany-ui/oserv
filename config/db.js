928ef49264edbe4fd6ddee0d74d6ce36851f727e
yusuf <you@example.com>
2025-11-05 13:26:03 +0700

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,8 +1,17 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const db = new Sequelize ("service_app","root","",{
-    host: "localhost",
+const db = new Sequelize(
+  process.env.DB_NAME,      // railway
+  process.env.DB_USER,      // root
+  process.env.DB_PASSWORD,  // password Railway
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
-});
+    logging: false,
+  }
+);
 
 export default db;
